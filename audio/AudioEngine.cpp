1bfe0b59844e98de51a8d9aeb4dfae28cee39365
Artur Wrona <arturwrona91@gmail.com>
2024-07-03 22:41:22 +0200

    Compiling with portaudiocpp as option

--- audio/AudioEngine.cpp
+++ audio/AudioEngine.cpp
@@ -1,30 +1,56 @@
 #include "audio/AudioEngine.h"
 #include "audio/Buffer.h"
+#ifdef USE_PORTAUDIOCPP
+#include <algorithm>
+#endif
+#ifdef DEBUG
 #include <iostream>
+#endif
 
 AudioEngine::AudioEngine(int nChannels, int nFrames, int nPeriods, int flags, int samplerate)
-: _nChannels(nChannels)
+: _interleaved(true)
+, _nChannels(nChannels)
 , _nFrames(nFrames)
 , _nPeriods(nPeriods)
 , _samplerate(samplerate)
 , _stream_flags(flags)
-, _output(nullptr)
-, _input(nullptr)
+//, _output(nullptr)
+//, _input(nullptr)
 , _running(false)
+#ifdef USE_PORTAUDIOCPP
+, _sampleformat(portaudio::FLOAT32)
 , _sys(nullptr)
-, _interleaved(true)
+#else
+, _sampleformat(paFloat32)
+, _paError(Pa_Initialize())
+, _outDev(paNoDevice)
+, _inDev(paNoDevice)
+, _stream(nullptr)
+#endif
 {
-    std::cout << "Portaudio instance inside AudioEngine c-tor: ";
+#ifdef USE_PORTAUDIOCPP
     _sys = &(portaudio::System::instance());
-    std::cout << (void*) _sys << std::endl;
-    initBuffers();    
+#else
+    if (!_interleaved)
+        _sampleformat |= paNonInterleaved;
+    else
+        _sampleformat &= ~paNonInterleaved;
+#endif
+    initBuffers();
 }
 
 AudioEngine::~AudioEngine()
 {
+#ifdef DEBUG
     std::cout << "Terminating audio engine" << std::endl;
+#endif
+#ifdef USE_PORTAUDIOCPP
     if (_sys)
         _sys->terminate();
+#else
+    if (_paError == paNoError)
+        Pa_Terminate();
+#endif
 }
 
 void AudioEngine::initBuffers()
@@ -37,8 +63,7 @@ void AudioEngine::initBuffers()
 
 void AudioEngine::startStream()
 {
-    
-    std::cout << "Starting stream" << std::endl;
+#ifdef USE_PORTAUDIOCPP
     portaudio::StreamParameters streamParams = portaudio::StreamParameters(
                          _inParams,
                          _outParams,
@@ -54,43 +79,127 @@ void AudioEngine::startStream()
     catch(...) {
         _running = false;
     }
-    
+#else
+    PaStreamParameters streamParams;
+    PaError err = Pa_OpenStream(
+          &_stream,
+          ((_stream_flags & STREAM_INPUT) ? &_inParams : nullptr),
+          ((_stream_flags & STREAM_OUTPUT) ? &_outParams : nullptr),
+          _samplerate,
+          _nFrames,
+          paClipOff,
+          AudioEngine::paStreamCallback,
+          this );
+    if (!err) {
+        err = Pa_StartStream(_stream);
+    }
+    _running = (!err);
+#endif
 }
 
 void AudioEngine::setupDefaultStreamParameters()
 {
+#ifdef USE_PORTAUDIOCPP
     if (!_sys) return;
-    std::cout << "Setup stream" << std::endl;
     if (_stream_flags & STREAM_OUTPUT) {
         _outParams = portaudio::DirectionSpecificStreamParameters(
-                         _sys->defaultOutputDevice(),
+                         *_outDev,
                          _nChannels,
                          portaudio::FLOAT32,
                          _interleaved,
-                         _sys->defaultOutputDevice().defaultLowOutputLatency(),
+                         _outDev->defaultLowOutputLatency(),
                          NULL);
     } else {
         _outParams = portaudio::DirectionSpecificStreamParameters::null();
     }
     if (_stream_flags & STREAM_INPUT) {
         _inParams  = portaudio::DirectionSpecificStreamParameters(
-                         _sys->defaultInputDevice(),
+                         *_inDev,
                          _nChannels,
-                         portaudio::FLOAT32,
+                         _sampleformat,
                          _interleaved,
-                         _sys->defaultInputDevice().defaultLowInputLatency(),
+                         _inDev->defaultLowInputLatency(),
                          NULL);
      } else {
          _inParams = portaudio::DirectionSpecificStreamParameters::null();
      }
+#else
+    if (_stream_flags & STREAM_OUTPUT) {
+        _outParams = {
+            _outDev,
+            _nChannels,
+            _sampleformat,
+            Pa_GetDeviceInfo( _outDev )->defaultLowOutputLatency,
+            nullptr
+        };
+    }
+    if (_stream_flags & STREAM_INPUT) {
+        _inParams = {
+            _inDev,
+            _nChannels,
+            _sampleformat,
+            Pa_GetDeviceInfo( _inDev )->defaultLowInputLatency,
+            nullptr
+        };
+     }
+#endif
+}
+
+void AudioEngine::setupHostApi(PaHostApiTypeId hostid)
+{
+#ifdef USE_PORTAUDIOCPP
+    auto apiFound = [hostid](portaudio::HostApi& api){
+        return api.typeId()==hostid;
+    };
+    auto it = std::find_if(_sys->hostApisBegin(), _sys->hostApisEnd(), apiFound);
+    if (it != _sys->hostApisEnd()) {
+        portaudio::HostApi *api = &(*it);
+        _outDev = &(api->defaultOutputDevice());
+        _inDev = &(api->defaultInputDevice());
+    } else {
+        setupDefaultDevice();
+    }
+    _samplerate = _outDev->defaultSampleRate();
+#else
+    bool apiFound = false;
+    int numDevices = Pa_GetDeviceCount();
+    for (int i = 0; i<numDevices; i++) {
+        const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(i);
+        auto api = Pa_GetHostApiInfo( deviceInfo->hostApi );
+        if (api->type == hostid) {
+            _inDev = api->defaultInputDevice;
+            _outDev = api->defaultOutputDevice;
+            apiFound = true;
+            break;
+        }
+    }
+    if (!apiFound)
+        setupDefaultDevice();
+    _samplerate = Pa_GetDeviceInfo( _outDev )->defaultSampleRate;
+#endif
+}
+
+void AudioEngine::setupDefaultDevice()
+{
+#ifdef USE_PORTAUDIOCPP
+    _outDev = &(_sys->defaultOutputDevice());
+    _inDev = &(_sys->defaultInputDevice());
+#else
+    _outDev = Pa_GetDefaultOutputDevice();
+    _inDev = Pa_GetDefaultInputDevice();
+#endif
 }
 
 void AudioEngine::stopStream()
 {
-    std::cout << "Stopping stream" << std::endl;
     _running = false;
+#ifdef USE_PORTAUDIOCPP
     _stream.stop();
     _stream.close();
+#else
+    Pa_StopStream(_stream);
+    Pa_CloseStream(_stream);
+#endif
 }
 
 void AudioEngine::readBuffer(void* data,unsigned int nFrames)
@@ -107,16 +216,26 @@ void AudioEngine::fillBuffer(const void* data,unsigned int nFrames)
     _input->write(in, nFrames*_nChannels);
 }
 
+int AudioEngine::paStreamCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer,
+        const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData)
+{
+    auto self = reinterpret_cast<AudioEngine*>(userData);
+    return self->handlePaStream(inputBuffer,
+                                outputBuffer,
+                                framesPerBuffer,
+                                timeInfo,
+                                statusFlags);
+}
+
 int AudioEngine::handlePaStream(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer,
         const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags)
 {
     if (_stream_flags & STREAM_OUTPUT)
         readBuffer(outputBuffer, framesPerBuffer);
-    
+
     if (_stream_flags & STREAM_INPUT)
         fillBuffer(inputBuffer, framesPerBuffer);
-    
-    
+
     return paContinue;
 }
 
--- audio/AudioEngine.h
+++ audio/AudioEngine.h
@@ -1,9 +1,11 @@
 #ifndef AUDIOENGINE_H
 #define AUDIOENGINE_H
+#ifdef USE_PORTAUDIOCPP
 #include "portaudiocpp/PortAudioCpp.hxx"
+#endif
+#include "portaudio.h"
 #include <vector>
 #include <memory>
-#include "audio/AudioSource.h"
 #include "audio/Buffer.h"
 
 #define STREAM_OUTPUT 1
@@ -15,15 +17,15 @@ class AudioEngine
 public:
     AudioEngine(int nChannels = 1, int nFrames = 512, int nPeriods = 1, int flags = STREAM_DUPLEX, int samplerate = 44100);
     ~AudioEngine();
-    
     int handlePaStream(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer,
         const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags);
+    static int paStreamCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer,
+        const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
     void initBuffers();
     void readBuffer(void*,unsigned int);
     void fillBuffer(const void*,unsigned int);
-    
-    AudioBuffer * output() { return _output; }
-    AudioBuffer * input() { return _input; }
+    AudioBuffer * output() { return _output.get(); }
+    AudioBuffer * input() { return _input.get(); }
     unsigned int outputBufferSize() const {
         return _output ? _output->capacity() : 0;
     }
@@ -35,6 +37,8 @@ public:
     void setupDuplex() { _stream_flags = (STREAM_INPUT | STREAM_OUTPUT); }
     void startStream();
     void setupDefaultStreamParameters();
+    void setupHostApi(PaHostApiTypeId hostid);
+    void setupDefaultDevice();
     void stopStream();
     void setSamplerate(unsigned int r) { _samplerate = r; }
     int samplerate() const { return _samplerate; }
@@ -44,44 +48,32 @@ public:
     void setChannelsNumber(unsigned int n) {
         _nChannels = n;
         resizeBuffer();
-        //_outParams.setNumChannels(_nChannels);
-        //_inParams.setNumChannels(_nChannels);
-        
     }
     void setPeriodsNumber(unsigned int n) {
         _nPeriods = n;
         resizeBuffer();
     }
-    
     bool running() const {
         return _running;
     }
-    
     void setInputBuffer(AudioBuffer* in = nullptr) {
         if (_input) {
-            delete _input;
-            _input = nullptr;
+            _input.reset();
         }
         if (in) {
-            _input = in;
+            _input = std::unique_ptr<AudioBuffer>(in);
         }
-        else _input = new AudioBuffer(_nFrames*_nChannels*_nPeriods);
-        
+        else _input = std::make_unique<AudioBuffer>(_nFrames*_nChannels*_nPeriods);
     }
-    
     void setOutputBuffer(AudioBuffer* out = nullptr) {
         if (_output) {
-             delete _output;
-             _output = nullptr;
+             _output.reset();
         }
         if (out) {
-            _output = out;
+            _output = std::unique_ptr<AudioBuffer>(out);
         }
-        else _output = new AudioBuffer(_nFrames*_nChannels*_nPeriods);
+        else _output = std::make_unique<AudioBuffer>(_nFrames*_nChannels*_nPeriods);
     }
-    
-    portaudio::System* paSystem() const { return _sys; }
-    
 private:
     void resizeBuffer() {
         _input->resize(_nFrames * _nChannels * _nPeriods);
@@ -89,22 +81,31 @@ private:
     }
 protected:
     const bool _interleaved;
+#ifdef USE_PORTAUDIOCPP
+    portaudio::SampleDataFormat _sampleformat;
     portaudio::AutoSystem autoSys;
     portaudio::System *_sys;
     portaudio::MemFunCallbackStream<AudioEngine> _stream;
-    //portaudio::StreamParameters _streamParams;
     portaudio::DirectionSpecificStreamParameters _outParams;
     portaudio::DirectionSpecificStreamParameters _inParams;
-    AudioBuffer* _input;
-    AudioBuffer* _output;
+    portaudio::Device *_outDev;
+    portaudio::Device *_inDev;
+#else
+    PaSampleFormat _sampleformat;
+    PaStream *_stream;
+    PaStreamParameters _outParams;
+    PaStreamParameters _inParams;
+    PaDeviceIndex _outDev;
+    PaDeviceIndex _inDev;
+    PaError _paError;
+#endif
+    std::unique_ptr<AudioBuffer> _input;
+    std::unique_ptr<AudioBuffer> _output;
     int _stream_flags;
-    unsigned int _nChannels;
+    int _nChannels;
     unsigned int _samplerate;
     unsigned int _nFrames;
     unsigned int _nPeriods;
     bool _running;
-    
-
 };
-
 #endif // AUDIOENGINE_H
